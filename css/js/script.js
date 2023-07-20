b4b519064ed73afabd1da8e9f49fbb8b350a1067
eliuth jimenez <elmartinezjm751@icloud.com>
2023-07-19 22:37:04 -0500

    cuarto committ implemantacion de las funciones en js para abrir y cerrar los modales de la imformacion de la formacion academica

--- css/js/script.js
+++ css/js/script.js
@@ -25,17 +25,22 @@ const figures = () =>{
 figures()
 
 
-
+//variables las cuales obtienen los id para ser usadas en las funciones 
 const firstHabilidad = document.querySelector('#conatiner-info-habilidad'),
+      towHabilidad = document.querySelector('#conatiner-info-habilidad2'),
       containerModal= document.querySelector('#container-modal'),
+      containerModal2 = document.querySelector('#container-modal2'),
       openButton = document.querySelector('#habilidad1'),
-      closeButton = document.querySelector('#boton-close');
+      openButton2 = document.querySelector('#habilidad2'),
+
+      closeButton = document.querySelector('#boton-close'),
+      closeButton2 = document.querySelector('#boton-close2');
       
 
       openButton.addEventListener('click', ()=>firstHabilidad.style.display  = 'flex' )
 
 
-
+//funcion qeu cierra el modal 
 const closeModal = ()=>{
     containerModal.classList.add('close');
 
@@ -44,4 +49,18 @@ const closeModal = ()=>{
         firstHabilidad.style.display  = 'none'
     }, 1000);
 }
+
+const closeModal2 = ()=>{
+    containerModal2.classList.add('close');
+
+    setTimeout(() =>{
+        containerModal2.classList.remove('close')
+        towHabilidad.style.display  = 'none'
+    }, 1000);
+}
+//llama la funcion que cierra el modal 
 closeButton.addEventListener('click', ()=>closeModal());
+closeButton2.addEventListener('click', ()=>closeModal2());
+
+
+openButton2.addEventListener('click', ()=>towHabilidad.style.display  = 'flex' )
